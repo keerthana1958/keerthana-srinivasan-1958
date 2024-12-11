b79d6bc3cc15c25488c49425ca15c865e68840b8
keerthana1958 <keerthana@qaoncloud.com>
2024-12-11 22:32:21 +0530

    Add files via upload

--- file upload.cy.js
+++ file upload.cy.js
@@ -1,4 +1,6 @@
-it('upload',()=>
+describe('Upload',()=>
+    {
+        it('upload',()=>
 
     {
      cy.visit("https://the-internet.herokuapp.com/upload")
@@ -10,3 +12,68 @@ it('upload',()=>
 
 
     })
+
+
+
+    it('rename',()=>
+
+        {
+         cy.visit("https://the-internet.herokuapp.com/upload")
+         cy.get("#file-upload").attachFile({filePath:'dummy.pdf',fileName:'rename.pdf'})
+         cy.get("#file-submit").click()
+         cy.wait(5000)
+         cy.get("div[class='example'] h3").should('have.text','File Uploaded!')
+    
+
+
+
+
+})
+it('drag-n-drop',()=>
+
+    {
+     cy.visit("https://the-internet.herokuapp.com/upload")
+     cy.get("#drag-drop-upload").attachFile('dummy.pdf',{subjectType: 'drag-n-drop'})
+     cy.get("#file-submit").click()
+     cy.wait(5000)
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+it('multi-upload',()=>
+
+    {
+     cy.visit("https://davidwalsh.name/demo/multiple-file-upload.php")
+     cy.get("#filesToUpload").attachFile(['dummy.pdf','dummy - Copy.pdf'])
+
+     cy.wait(5000)
+
+
+
+    })
+    it.only('shadow dom',()=>
+
+        {
+         cy.visit("https://www.htmlelements.com/demos/fileupload/shadow-dom/")
+         cy.get('.smart-browse-input',{includeShadowDom:true}).attachFile('dummy.pdf')
+    
+         cy.wait(5000)
+    
+    
+    
+        })
+    
+
+})
